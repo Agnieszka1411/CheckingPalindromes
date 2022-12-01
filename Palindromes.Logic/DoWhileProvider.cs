b4a59135a55ab08c20dad5319ef77118cc638c9f
Agnieszka Zmora <azmora@pgs-soft.com>
2022-12-01 10:37:17 +0100

    Adding new clases handled palindromes.

---
+++ Palindromes.Logic/DoWhileProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindromes.Logic
+{
+    public class DoWhileProvider : InvalidCharacters, IPalindormeProvider
+    {
+        public bool CheckString(string stringToCheck)
+        {
+            if (string.IsNullOrWhiteSpace(stringToCheck))
+            {
+                return false;
+            }
+
+            var cleanedString = GetCleanedString(stringToCheck.ToLower());
+            int index = cleanedString.Length - 1;
+            int i = 0;
+
+            do
+            {
+                if (cleanedString[i] == cleanedString[index])
+                {
+                    i++;
+                    index--;
+                }
+                else
+                    return false;
+            } while (i <= index);
+
+            return true;
+        }
+    }
+}
