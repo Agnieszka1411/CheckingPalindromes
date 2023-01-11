e7bb973ab9824de4b1b5f71d56b4bc348167c9ec
Agnieszka Zmora <azmora@pgs-soft.com>
2023-01-11 18:38:58 +0100

    Fix merge

---
+++ Palindromes.Logic/Providers/DoWhileProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace Palindromes.Logic
+{
+    public class DoWhileProvider : BaseProvider
+    {
+        public override bool CheckString(string stringToCheck)
+        {
+            if (string.IsNullOrWhiteSpace(stringToCheck))
+                return false;
+
+            var cleanedString = GetCleanedString(stringToCheck.ToLower());
+            var index = cleanedString.Length - 1;
+            var i = 0;
+
+            do
+            {
+                if (cleanedString[i] == cleanedString[index])
+                {
+                    i++;
+                    index--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            while (i <= index);
+
+            return true;
+        }
+    }
+}
---
+++ Palindromes.Logic/Providers/ForeachProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace Palindromes.Logic
+{
+    public class ForeachProvider : BaseProvider
+    {
+        public override bool CheckString(string stringToCheck)
+        {
+            if (string.IsNullOrWhiteSpace(stringToCheck))
+                return false;
+
+            var cleanedString = GetCleanedString(stringToCheck.ToLower());
+
+            var index = cleanedString.Length - 1;
+
+            foreach (var element in cleanedString)
+            {
+                if (element == cleanedString[index])
+                    index--;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
